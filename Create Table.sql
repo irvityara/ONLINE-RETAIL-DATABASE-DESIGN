9c693556c9747d710e320e59fe0e4c016a541bda
Ipikk <irvityara456@gmail.com>
2023-11-09 09:23:08 +0700

    create table pelanggan

---
+++ Create Table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE pelanggan(
+    id_pelanggan INTEGER PRIMARY KEY NOT NULL auto_increment,
+    nama_pelanggan VARCHAR NOT NULL,
+    alamat_pelanggan VARCHAR NOT NULL,
+    email_pelanggan VARCHAR NOT NULL,
+);
+
