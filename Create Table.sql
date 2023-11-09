d8dd4f5b08cb1dab052c598f996244fec2318c06
Ipikk <irvityara456@gmail.com>
2023-11-09 09:32:21 +0700

    create table

--- Create Table.sql
+++ Create Table.sql
@@ -2,7 +2,7 @@ CREATE TABLE pelanggan(
     id_pelanggan INTEGER PRIMARY KEY NOT NULL auto_increment,
     nama_pelanggan VARCHAR NOT NULL,
     alamat_pelanggan VARCHAR NOT NULL,
-    email_pelanggan VARCHAR NOT NULL,
+    email_pelanggan VARCHAR NOT NULL
 );
 
 CREATE TABLE transaksi(
@@ -10,7 +10,7 @@ CREATE TABLE transaksi(
     id_pelangganFK INTEGER REFERENCES pelanggan(id_pelanggan) ON DELETE CASCADE ON UPDATE CASCADE,
     tanggal_transaksi DATE NOT NULL,
     jumlah_transaksi INTEGER NOT NULL,
-    jenis_transaksi VARCHAR NOT NULL,
+    jenis_transaksi VARCHAR NOT NULL
 );
 
 CREATE TABLE produk(
@@ -19,5 +19,5 @@ CREATE TABLE produk(
     nama_produk VARCHAR NOT NULL,
     kategori_produk VARCHAR NOT NULL,
     jumlah_produk INTEGER NOT NULL,
-    harga_produk INTEGER NOT NULL,
+    harga_produk INTEGER NOT NULL
 );
