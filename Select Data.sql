67618fe6fc9cc03be30bab70099b54a4f1f18ad4
Ipikk <irvityara456@gmail.com>
2023-11-09 20:08:16 +0700

    update data

---
+++ Select Data.sql
@@ -0,0 +1,6 @@
+SELECT * FROM pelanggan;
+SELECT * FROM transaksi;
+SELECT * FROM produk;
+SELECT * FROM pelanggan_beli_produk;
+
+
---
+++ Update Data.sql
@@ -0,0 +1,4 @@
+
+-- mengubah alamat pelanggan dengan id pelanggan "1001" menjadi JL. Jambu Air No.20
+UPDATE pelanggan SET alamat_pelanggan="Jl. Jambu Air No.100" WHERE id_pelanggan="1001";
+
