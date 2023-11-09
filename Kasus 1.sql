5f18158eb635d44ebb55fd2199bdb1c3e82145b8
Ipikk <irvityara456@gmail.com>
2023-11-09 20:47:26 +0700

    kasus 1

---
+++ Kasus 1.sql
@@ -0,0 +1,1 @@
+SELECT pelanggan.id_pelanggan, pelanggan.nama_pelanggan, produk.nama_produk FROM pelanggan JOIN pelanggan_beli_produk ON pelanggan.id_pelanggan=pelanggan_beli_produk.id_pelangganFK JOIN produk ON pelanggan_beli_produk.no_produkFK=produk.no_produk WHERE pelanggan.id_pelanggan="1015";
